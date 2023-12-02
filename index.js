597be84ae0e9d9d3a4cdeb6393d7ceba95d6b00e
tiya reba <cvjino@gmail.com>
2023-12-01 21:27:41 -0800

    zxzx

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const express=require("express")
+const express=require("express");
+const cors=require("cors")
+
+const studmodel = require("./model/student");
 
 const app=new express();
 
-app.listen(3005,(request,response)=>{
-    console.log("Port is running in 3005")
-})
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cors());
+
+
+
 
 app.get('/',(request,response)=>{
     response.send("hi database")
+})
+
+//For Saving student data
+
+app.post('/snew',(request,response)=>{
+    new studmodel(request.body).save();
+    response.send("Record saved Sucessfully")
+})
+
+//For retriving student data
+app.get('/sview',async(request,response)=>{
+    var data = await studmodel.find();
+    response.send(data)
+})
+
+//For update status of student -delete
+app.put('/updatestatus/:id',async(request,response)=>{
+    let id = request.params.id
+    await studmodel.findByIdAndUpdate(id,{$set:{Status:"INACTIVE"}})
+    response.send("Record Deleted")
+})
+
+//For modifing the details student
+app.put('/sedit/:id',async(request,response)=>{
+    let id = request.params.id
+    await studmodel.findByIdAndUpdate(id,request.body)
+    response.send("Record updated")
+})
+
+
+app.listen(3005,(request,response)=>{
+    console.log("Port is running in 3005")
 })
--- model/student.js
+++ model/student.js
@@ -1,5 +1,17 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://nigiljoseph2017:nigil@cluster0.gb23b3l.mongodb.net/?retryWrites=true&w=majority")
+
+mongoose.connect("mongodb+srv://nigiljoseph2017:nigil@cluster0.gb23b3l.mongodb.net/db1?retryWrites=true&w=majority")
 .then(()=>{console.log("Db connected")})
 .catch(err=>console.log(err));
+
+let sc=mongoose.Schema;
+const studschema = new sc({
+    Admno:String,
+    Sname:String,
+    Age:Number,
+    Status:String
+});
+
+var studmodel =mongoose.model("Student",studschema)
+module.exports =studmodel;
 
